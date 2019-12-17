29cb1341c6952ab6f58c31e532dfe937a7aa42a6
Shivanand LC <shivanand7210@gmail.com>
2019-12-17 22:40:24 +0530

    second logger commented

--- src/main/java/com/example/demo/JenkinstestApplication.java
+++ src/main/java/com/example/demo/JenkinstestApplication.java
@@ -15,7 +15,7 @@ public class JenkinstestApplication {
 	@PostConstruct
 	public void init() {
 		logger.info("Application started........");
-		logger.info("Application executed........");
+		//logger.info("Application executed........");
 	}
 	
 	public static void main(String[] args) {
